2dfc363875c598754d38d7d42d7420b74a360762
SeungwooYun <103883076+SeungwooYun@users.noreply.github.com>
2024-10-03 14:30:52 +0000

    till 55

--- app.js
+++ app.js
@@ -1,27 +1,9 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const app = express();
+const http = require('http');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require('./routes/shop');
+const routes = require("./routes");
 
-// body-parser은 함수 끝에 이미 next()가 적혀있음 그래서 따로 안해도 됨
-app.use(bodyParser.urlencoded({extended: true}));
+console.log(routes.someText);
 
+const server = http.createServer(routes.handler);
 
-// app.use(('/', req, res, next)=>{
-// 라고 되어 있으면 '/' 뿐 아니라 /로 시작하는 모든 것에 적용됨 
-// 그러니까 /abc를 쓰고 싶으면 / 위쪽에서 미들웨어 등록해줘야하는 거임
-// get, post, put etc.. 는 정확하게 '' 안의 루트와 매치돼야 함. use 만 ~로 시작하는 거임
-
-
-// 이 라우터 첫번째 아규먼트에 '/admin' 넣어주면 두번째 /admin/두번째 아규먼트 필드로 들어가게됨  
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-app.use((req, res, next)=>{
-    res.status(404).send("<h1>Page not found</h1>")
-});
-// 에러처리
-
-
-app.listen(3000);
+server.listen(3000); 
--- routes.js
+++ routes.js
@@ -1,48 +1,42 @@
 const fs = require('fs');
 
-
 const requestHandler = (req, res) => {
-    res.setHeader("Content-Type", "text/html");
     const url = req.url;
-    const method = req.method
-    if(url==="/"){
-        res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write("<body><form action='/message' method='POST'><input type='text' name='message'></input><button type='submit'>Send</button></form></body>");
-    res.write("</html>");
-    return res.end();    
-    }
-    if(url==="/message" && method==="POST"){
-    
+    const method = req.method;
+    console.log(url, method, req.headers);
+if(url==='/'){
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>'); 
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send123</button></input></form></body>');
+    res.write('</html>'); 
+    return res.end();
+} 
+
+if(url==='/message' && method==='POST'){
     const body = [];
-    req.on("data", (chunk)=>{
-    console.log(chunk)
-    body.push(chunk)
+    req.on('data', (chunk)=>{
+        console.log(chunk)
+        body.push(chunk);
+    });
+    return req.on('end', ()=>{
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split("=")[1];
+        console.log(message);
+        fs.writeFile('message.txt', message,  err => {
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        });
     })
-    
-    // 리스너를 달아주고 데이터를 모음
-    // data이벤트는 모든 chunk 가 준비될 때마다 fired 됨
-    req.on("end", ()=>{
-    const parsedBody = Buffer.concat(body).toString();
-    const message = parsedBody.split("=")[1];
-    console.log(parsedBody);
-    //  Buffer 오브젝트는 node.js에서 그냥 제공해주는 거임 
-    // body를 parsing 하는 것임 
-    
-    fs.writeFileSync("message.txt", message)
-    })
-    // chunk 다 모으고 나면 end 안에서 buffer 하는 거임
-    
-    res.statusCode = 302;
-    res.setHeader('Location', "/");
-    return res.end()
-    }
-    res.write("<html>");
-    res.write("<head><title>My first page</title></head>");
-    res.write("<body><h1>Hello, this is my first html respnse</h1></body>");
-    res.write("</html>");
-    res.end();
-    // end 이후에는 write 더 쓰면 안됨. 오류.
+}
+res.setHeader('Content-Type', 'text/html');
+res.write('<html>');
+res.write('<head><title>My First Page</title></head>');
+res.write('<body><h1>Hello from Node.js Server</h1></body>');
+res.write('</html>');
+res.end();
 }
 
-module.exports = requestHandler;
+exports.someText = "some hard coded text"
+exports.handler = requestHandler
