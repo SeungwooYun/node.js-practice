79d99eb5a213d75912d3ee090fbe9cc3466d486d
SeungwooYun <103883076+SeungwooYun@users.noreply.github.com>
2024-10-06 17:22:32 +0000

    pug template added

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,19 @@
+const path = require('path');
 const express = require("express");
 
+const rootDir = require("../util/path");
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next)=>{
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</form>')
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
     });
 
 router.post('/product', (req, res, next)=>{
-    console.log(req.body);
+    products.push({ title: req.body.title, });
     res.redirect('/');
 })
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
