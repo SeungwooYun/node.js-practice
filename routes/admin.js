02dff23217018d86a9ffad3201baa3eef6639a4f
Seungwoo Yun <yoooonseungwoo@gmail.com>
2024-10-12 01:38:18 +0800

    100

---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const express = require('express');
+
+const ProductsController = require("../controllers/products");
+
+const router = express.Router();
+
+
+
+// /admin/add-product => GET
+router.get('/add-product', ProductsController.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', ProductsController.postAddProduct);
+
+module.exports = router;
